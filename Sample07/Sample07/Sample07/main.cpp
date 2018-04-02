665323f146c8dc7af94915a69c9db15cfea4dcba
Kizirinov <Kazarinov_AV@mail.ru>
2018-04-02 11:27:28 +0300

    Remove files

---
+++ Sample07/Sample07/Sample07/main.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "Sample07.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	Sample07 w;
+	w.show();
+	return a.exec();
+}
