3342ab88d7688bc9b559a0e724cf05d071f59229
Sergey Shambir <sergey.shambir@ispringsolutions.com>
2018-05-07 06:39:59 +0300

    Добавил textToImage - функцию растеризации текста в изображение

---
+++ libs/libplatform/TextToImage.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "TextToImage.h"
+#include <QtGui/QFontMetrics>
+#include <QtGui/QPainter>
+
+namespace platform
+{
+
+QImage platform::textToImage(const QFont &font, const QString &text)
+{
+	QFontMetrics metrics(font);
+	QSize size{ metrics.width(text), metrics.height() };
+	return textToImage(font, size, text);
+}
+
+QImage platform::textToImage(const QFont &font, const QSize &size, const QString &text)
+{
+	QImage image{ size.width, size.height, QImage::Format_RGBA8888 };
+	QPainter painter(&image);
+	painter.setPen(QPen(Qt::black));
+	painter.setFont(font);
+	painter.drawText(image.rect(), Qt::AlignCenter, text);
+
+	return image;
+}
+
+} // namespace platform
